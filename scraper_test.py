5e4118bfcde570b2f5071c9fa997b973558ced67
Iain Emsley <iain.emsley@warwick.ac.uk>
2020-11-23 12:42:51 +0000

    Adding in more tests

--- scraper_test.py
+++ scraper_test.py
@@ -1,5 +1,5 @@
 from itunes_app_scraper.scraper import AppStoreScraper
-from itunes_app_scraper.util import AppStoreException
+from itunes_app_scraper.util import AppStoreException, AppStoreCollections, AppStoreCategories
 import pytest
 import os
 
@@ -39,3 +39,30 @@ def test_log_file_write_message():
     assert "test" in fh.read()
     fh.close()
     os.remove('gb_log.txt')
+
+def test_country_code_does_exist():
+    scraper = AppStoreScraper()
+    assert scraper.get_store_id_for_country('gb') == 143444
+
+def test_country_code_does_not_exist():
+    scraper = AppStoreScraper()
+    with pytest.raises(AppStoreException, match="Country code not found for XZ"):
+        scraper.get_store_id_for_country('xz')
+
+def test_category_exists():
+    category = AppStoreCategories()
+    assert category.BOOKS == 6018
+
+def test_category_does_not_exist():
+    category = AppStoreCategories()
+    with pytest.raises(AttributeError, match="'AppStoreCategories' object has no attribute 'METHOD'"):
+        category.METHOD
+
+def test_collection_exists():
+    collection = AppStoreCollections()
+    assert collection.NEW_IOS == 'newapplications'
+
+def test_collection_does_not_exist():
+    collection = AppStoreCollections()
+    with pytest.raises(AttributeError, match="'AppStoreCollections' object has no attribute 'NOTHING'"):
+        collection.NOTHING

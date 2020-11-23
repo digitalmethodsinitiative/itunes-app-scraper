238c3f913ebbf1371ac7cd3da97302ae69b8d40b
Iain Emsley <iain.emsley@warwick.ac.uk>
2020-11-23 12:02:39 +0000

    Updating unit tests

--- scraper_test.py
+++ scraper_test.py
@@ -1,5 +1,7 @@
 from itunes_app_scraper.scraper import AppStoreScraper
+from itunes_app_scraper.util import AppStoreException
 import pytest
+import os
 
 def test_term_no_exception():
     scraper = AppStoreScraper()
@@ -8,5 +10,32 @@ def test_term_no_exception():
 
 def test_no_term_gives_exception():
     scraper = AppStoreScraper()
-    with pytest.raises('AppStoreException'):
+    with pytest.raises(AppStoreException, match = "No term was given"):
         scraper.get_app_ids_for_query("", country="gb", lang="en")
+
+def test_no_invalid_id_gives_exception():
+    scraper = AppStoreScraper()
+    with pytest.raises(AppStoreException, match = "No app found with ID 872"):
+        scraper.get_app_details('872')
+
+def test_no_invalid_id_in_multiple_is_empty():
+    scraper = AppStoreScraper()
+    assert len(list(scraper.get_multiple_app_details(['872']))) == 0
+
+def test_no_invalid_id_in_multiple_writes_log():
+    scraper = AppStoreScraper()
+    scraper.get_multiple_app_details(['872'])
+    assert os.path.exists("nl_log.txt")
+    fh = open('nl_log.txt')
+    assert "No app found with ID 872" in fh.read()
+    fh.close()
+    os.remove('nl_log.txt')
+
+def test_log_file_write_message():
+    scraper = AppStoreScraper()
+    scraper._log_error("gb","test")
+    assert os.path.exists("gb_log.txt")
+    fh = open('gb_log.txt')
+    assert "test" in fh.read()
+    fh.close()
+    os.remove('gb_log.txt')
